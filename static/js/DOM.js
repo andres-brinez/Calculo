ec66b2a1b75811a0b0d0059192c19fd283d1ab43
Andres Briñez <brinezlopez08@gmail.com>
2023-03-31 00:33:49 -0500

    Selectores del dom

---
+++ static/js/DOM.js
@@ -0,0 +1,7 @@
+const widthInput = document.getElementById("width");
+const heightInput = document.getElementById("height");
+const radiusInput = document.getElementById("radius");
+const calculateBtn = document.getElementById("calculate-btn");
+const result = document.getElementById("result");
+const shapeImage = document.getElementById("shape-image");
+const radiusContainer = document.getElementById("radius-container");
